e10e8e88b014a90dc75dd89507971f7d22962287
akash9676 <akashnaidu545gmail.com>
2022-01-16 13:39:09 +0530

    index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,24 @@
 module.exports = {
-  purge: [
-    './public/**/*.html',
-    require('@themesberg/flowbite/plugin')
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ["./public/**/*.html", require("@themesberg/flowbite/plugin")],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			width: {
+				mc: "max-content",
+			},
+			spacing: {
+				88: "21rem",
+			},
+			borderRadius: {
+				"40xl": "20rem",
+			},
+			scale: {
+				200: "2",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
