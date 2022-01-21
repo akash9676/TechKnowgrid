46fa6e7ae6c91dc564378f7d08840126633b3260
akash9676 <akashnaidu545@gmail.com>
2022-01-21 12:53:14 +0530

    techknowgrid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-	purge: ["./public/**/*.html", require("@themesberg/flowbite/plugin")],
+	content: ["./src/**/*.{html,js}"],
+	purge: ["./public/**/*.html"],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
+			color: {
+				main : "F5272A", // work-sans-700 30
+				secondary: "2491F3", // 30
+				com : "525761" //montserat-600 ,italic  14
+			},
 			width: {
 				mc: "max-content",
 			},
